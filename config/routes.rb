1f64f24cb686d278fece551b2d1522339026d16e
Raul Barroso <barroso_raul@ymail.com>
2017-12-27 20:20:39 +0000

    Added main pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about_us', to: 'pages#about_us'
+  get 'buy', to: 'pages#buy'
+  get 'rent', to: 'pages#rent'
+  get 'markets', to: 'pages#markets'
+  get 'contact_us', to: 'pages#contact_us'
 end
