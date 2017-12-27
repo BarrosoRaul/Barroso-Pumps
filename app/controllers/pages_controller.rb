032533a2648af56e87b109d304d56f662c74b44f
Raul Barroso <barroso_raul@ymail.com>
2017-12-27 19:38:02 +0000

    Gems, layout, stylesheets

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,18 @@ class PagesController < ApplicationController
   def home
   end
   
-  def about
+  def about_us
+  end
+  
+  def buy
+  end
+  
+  def rent
+  end
+  
+  def markets
+  end
+  
+  def contact_us
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  get 'about', to: 'pages#about'
+  get 'about_us', to: 'pages#about_us'
 end
